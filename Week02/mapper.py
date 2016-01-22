37b004fdf829a6fdec5bd4b643f8261ba59ef4fc
Miki Seltzer <miki.seltzer@berkeley.edu>
2016-01-21 23:04:36 -0800

    Work on HW2.2, read fields appropriately

--- Week02/mapper.py
+++ Week02/mapper.py
@@ -4,6 +4,7 @@
 ## Description: mapper code for HW2.2
 
 import sys
+import string
 
 # Get the user-specified word
 keyword = sys.argv[1]
@@ -12,7 +13,15 @@ keyword = sys.argv[1]
 for line in sys.stdin:
     
     # Strip white space from line, then split
-    words = line.strip().split()
+    # Some records are malformed -- if there is a 4th field, use it
+    fields = line.strip().split('\t')
+    subj = fields[2].translate(string.maketrans("",""), string.punctuation)
+    if len(fields) == 4:
+        body = fields[3].translate(string.maketrans("",""), string.punctuation)
+    else:
+        body = ""
+    words = subj + " " + body
+    words = words.split()
     
     # Loop through words
     # If it matches the keyword, write it to file
