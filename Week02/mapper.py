8d31ff78ffdb5f8b52500efd3f86fc2022ff925d
Miki Seltzer <miki.seltzer@berkeley.edu>
2016-01-25 20:54:02 -0800

    Add merge sort section

--- Week02/mapper.py
+++ Week02/mapper.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 ## mapper.py
 ## Author: Miki Seltzer
-## Description: mapper code for HW2.3
+## Description: mapper code for HW2.1
 
 import sys
-import string
-import re
 
 # Our input comes from STDIN (standard input)
 for line in sys.stdin:
-    
-    # Replace delimiter
-    line = line.replace('\n', '')
-    fields = line.split('\t')
-    print '%s^%s^%s^%s^%s' % (fields[0], fields[1], fields[2], fields[3], fields[4])
+    # In this case, we do not need to map the input to anything
+    key, value = line.strip().split(',')
+    print '%s\t%s' % (int(key), value)
